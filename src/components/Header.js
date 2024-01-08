4e1d6e0e516e187d4c0912a853507732c1f6fca9
Kalp Patel <kalppatel@Kalps-MacBook-Air.local>
2024-01-08 13:22:30 +0530

    Adding Feedback form for user

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,49 +8,30 @@ import Home from "./Routing/Home";
 function Header() {
   return (
     <>
-     <div className="main-route">
-      <Router>
-        <Navbar expand="lg" className="main_nav sticky-top">
-          <Navbar.Brand as={Link} to="/">Sorting Vizualization</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link as={Link} to="/Home">Home</Nav.Link>
-              <Nav.Link as={Link} to="/About">About</Nav.Link>
-              <Nav.Link as={Link} to="/Contact">Contact US</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <div className="main-route">
-          <Routes>
-            <Route path="Home" element={<Home/>} />
-            <Route path="About" element={<About/>} />
-            <Route path="Contact" element={<Contact />} />
-          </Routes>
-        </div>
-      </Router>
-    </div>
+      <div className="main-route">
+        <Router>
+          <Navbar expand="lg" className="main_nav sticky-top">
+            <Navbar.Brand as={Link} to="/">Sorting Visualization</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/About">About</Nav.Link>
+                <Nav.Link as={Link} to="/Contact">Contact US</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <div className="main-route">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/About" element={<About />} />
+              <Route path="/Contact" element={<Contact />} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
     </>
-    
-
-    // <Navbar expand="lg" className="bg-body-tertiary">
-    //   <Container fluid>
-    //     <Navbar.Brand href="/">Sorting Vizualization</Navbar.Brand>
-    //     <Navbar.Toggle aria-controls="navbarScroll" />
-    //     <Navbar.Collapse id="navbarScroll">
-    //       <Nav
-    //         className="me-auto my-2 my-lg-0 me-auto ms-auto"
-    //         style={{ maxHeight: '100px' }}
-    //         navbarScroll
-    //       >
-    //         <Nav.Link href="/">Home</Nav.Link>
-    //         <Nav.Link href="#action2">About Us</Nav.Link>
-    //         <Nav.Link href="#Contact.js">Contact Us</Nav.Link>
-    //       </Nav>
-    //     </Navbar.Collapse>
-    //   </Container>
-    // </Navbar>
   );
 }
 
-export default Header
+export default Header;
--- src/components/Routing/Home.js
+++ src/components/Routing/Home.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import { Box, Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import Visualiser from "../Visualiser";
+import Sidebar from "../Sidebar"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
+  const [arr, setArr] = useState([]);
+  const [generating, setGenerating] = useState(false);
+  const [sorting, setSorting] = useState(false);
+
+  const generateArr = (formState) => {
+    setGenerating(true);
+    setTimeout(() => {
+      let newArr = [];
+      while (newArr.length <= formState.size) {
+        let random = Math.floor(Math.random() * (200 - 10) + 10);
+        if (newArr.indexOf(random) === -1) {
+          newArr.push(random);
+        }
+      }
+      setArr([...newArr]);
+      setGenerating(false);
+    }, 500);
+  };
+
+  useState(() => {
+    generateArr({ size: 9 });
+  }, []);
+  console.log(arr);
+
+  const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+  const sort = async () => {
+    setSorting(true);
+
+    let newArr = [...arr];
+    const n = newArr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (newArr[j] > newArr[j + 1]) {
+          // swap newArr[j] and newArr[j+1]
+          let temp = newArr[j];
+          newArr[j] = newArr[j + 1];
+          newArr[j + 1] = temp;
+
+          setArr([...newArr]); // Update the state to trigger a re-render
+          await sleep(500); // Introduce a delay for visualization
+        }
+      }
+    }
+
+    setSorting(false);
+  };
+
   return (
-    <div>Home</div>
-  )
+    <>
+      <Box p={"4"}>
+        <Flex gap={"4"}>
+          <Sidebar
+            generateArr={generateArr}
+            generating={generating}
+            sorting={sorting}
+            sort={sort}
+          />
+          <Visualiser data={arr} />
+        </Flex>
+      </Box>
+    </>
+  );
 }
 
-export default Home
+export default Home;
